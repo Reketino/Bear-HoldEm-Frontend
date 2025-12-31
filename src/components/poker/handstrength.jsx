0911c87111b9a447558f37ff0f97e9353f9236a9
Reketino <bjornevensk8@gmail.com>
2025-12-31 10:40:46 +0100

    changed position of startbutton + removed unsued components

--- src/components/poker/handstrength.jsx
+++ src/components/poker/handstrength.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 
-export default function HandStrength({ showdown,strength }) {
+export default function HandStrength({ strength }) {
   if (!strength) return null;
 
   return (
